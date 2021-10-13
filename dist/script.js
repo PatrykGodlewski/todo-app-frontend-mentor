91ea62f50d7465e36e051437886e7f57440be536
Godlew <pgodlewski099@gmail.com>
2021-10-13 09:33:47 +0200

    added filter function, added checked function, added clear completed function, improved visuals of chceck and text

--- dist/script.js
+++ dist/script.js
@@ -1,13 +1,28 @@
-const todoInput = document.getElementById("todo--input");
-const todoSubmit = document.getElementById("todo--submit");
-const todoList = document.getElementById("todo--list");
+const todoInput = document.getElementById("todo--input"),
+  todoSubmit = document.getElementById("todo--submit"),
+  todoList = document.getElementById("todo--list"),
+  clearButton = document.getElementById("todo--button--clear"),
+  filterButtonAll = document.getElementById("All"),
+  filterButtonActive = document.getElementById("Active"),
+  filterButtonCompleted = document.getElementById("Completed");
 
 window.onload = onLoadSetTodo();
+
+filterButtonAll.addEventListener("click", filterAll, false);
+filterButtonActive.addEventListener("click", filterActive, false);
+filterButtonCompleted.addEventListener("click", filterCompleted, false);
+
+clearButton.addEventListener("click", clearCompleted, false);
 todoInput.addEventListener("keypress", addTodo, false);
+todoList.addEventListener("click", todoComplete, false);
 
 function addTodo(e) {
   e.preventDefault();
   if (e.key === "Enter") {
+    if (todoInput.value === "" || null || undefined) {
+      throw "Input is empty";
+      ///element div with class thas tells error
+    }
     ///Creating elems
     const li = document.createElement("li"),
       btn = document.createElement("button"),
@@ -33,6 +48,8 @@ function addTodo(e) {
     storageTodo(todoInput.value);
     //Seting inpust value to ""
     todoInput.value = "";
+    ///check counter
+    todoCounter();
   } else {
     todoInput.value += e.key;
   }
@@ -69,16 +86,76 @@ function onLoadSetTodo() {
     li.appendChild(wrap);
 
     todoList.appendChild(li);
+    ///check counter
+    todoCounter();
   }
 }
 
 function removeTodo(e) {
   ///Remove todo localStorage
-  lis = todoList.getElementsByTagName("li");
-  index = [...lis].indexOf(e.target.parentNode);
+  let lis = todoList.getElementsByTagName("li");
+  let index = [...lis].indexOf(e.target.parentNode.parentNode);
   let todos = JSON.parse(localStorage.getItem("tasks") || "[]");
   todos.splice(index, 1);
   localStorage.setItem("tasks", JSON.stringify(todos));
   ///Remove todo visual
   e.target.parentNode.parentNode.remove();
+  ///check counter
+  todoCounter();
+}
+function todoCounter() {
+  let lis = todoList.getElementsByTagName("li");
+  let lastIndex = [...lis].length;
+
+  const counterTag = document.getElementById("todo--counter");
+  counterTag.innerText = lastIndex + " ";
+}
+
+function todoComplete(e) {
+  if (e.target.classList.value === "todo--circle") {
+    e.target.classList = "todo--circle__completed";
+    e.target.parentNode.style.textDecoration = "line-through";
+    e.target.parentNode.style.color = "hsl(235, 19%, 35%)";
+  } else if (e.target.classList.value === "todo--circle__completed") {
+    e.target.classList = "todo--circle";
+    e.target.parentNode.style.textDecoration = "";
+    e.target.parentNode.style.color = "";
+  }
+}
+
+function clearCompleted() {
+  let lis = todoList.getElementsByTagName("li");
+  let length = [...lis].length;
+  let c = 0;
+  for (let i = 0; i < length; i++) {
+    if (lis[0 + c].children[0].classList.value === "todo--circle__completed") {
+      lis[0 + c].remove();
+      let todos = JSON.parse(localStorage.getItem("tasks") || "[]");
+      todos.splice(length, 1);
+      localStorage.setItem("tasks", JSON.stringify(todos));
+    } else {
+      c++;
+    }
+    todoCounter();
+  }
+}
+function filterAll() {
+  let lis = todoList.getElementsByTagName("li");
+  for (let i in [...lis]) {
+    lis[i].style.display = "";
+  }
+}
+function filterActive() {
+  ///reset styles
+  filterAll();
+  //filter completed
+  let completed = document.getElementsByClassName("todo--circle__completed");
+  for (let i in [...completed]) completed[i].parentNode.style.display = "none";
+}
+function filterCompleted() {
+  ///reset styles
+  filterAll();
+  //filter active
+  let active = document.getElementsByClassName("todo--circle");
+  for (let i in [...active]) active[i].parentNode.style.display = "none";
 }
